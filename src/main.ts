fb96d3b15ecd76ce38e008b803634146b9303723
Hishmathdeen <HishmathD@fortude.co>
2025-01-13 16:48:18 +0530

    Full Service Implementation

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      level: 'debug',
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple(),
+          ),
+        }),
+        new winston.transports.File({ filename: 'serviceA.log' }),
+      ],
+    }),
+  });
+
+  await app.listen(3000);
+  Logger.log('Service A is running on port 3000');
+}
+
+bootstrap();
