ccf5954a17b8c3f13ef22c91c513375351ac0309
Hishmathdeen <HishmathD@fortude.co>
2025-01-17 13:58:19 +0530

    winston log formarter

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,25 @@ import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      level: 'debug',
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.printf(({ level, message, timestamp, context }) => {
+              return JSON.stringify({
+                timestamp,
+                level,
+                context: context || 'A-SERVICE',
+                message,
+              });
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
 
   await app.listen(3000);
   Logger.log('Service A is running on port 3000');
